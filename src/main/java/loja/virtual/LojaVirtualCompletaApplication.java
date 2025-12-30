bf9ad44dbcf976a9779de8a51dfd6af0bbd2fa86
Bruno <bruno17henrique@gmail.com>
2025-12-30 20:43:51 -0300

    primeiro commit do projeto 30/12/2026

---
+++ src/main/java/loja/virtual/LojaVirtualCompletaApplication.java
@@ -0,0 +1,13 @@
+package loja.virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualCompletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualCompletaApplication.class, args);
+	}
+
+}
