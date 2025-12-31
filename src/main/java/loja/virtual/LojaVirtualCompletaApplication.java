94db68c527933bfa6600d7236f128285da28e49a
Bruno <bruno17henrique@gmail.com>
2025-12-30 21:37:21 -0300

    criado classe de marca produto

--- src/main/java/loja/virtual/LojaVirtualCompletaApplication.java
+++ src/main/java/loja/virtual/LojaVirtualCompletaApplication.java
@@ -2,8 +2,10 @@ package loja.virtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "LojaVirtualCompletaApplication.model")
 public class LojaVirtualCompletaApplication {
 
 	public static void main(String[] args) {
