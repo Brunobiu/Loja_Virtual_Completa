fa38864908ed4283eb4b2e18215fd46979fa4039
Bruno <bruno17henrique@gmail.com>
2026-01-03 14:27:28 -0300

    Commit classe categoria produto

--- src/main/java/LojaVirtualCompletaApplication/model/MarcaProduto.java
+++ src/main/java/LojaVirtualCompletaApplication/model/MarcaProduto.java
@@ -1,25 +1,23 @@
 package LojaVirtualCompletaApplication.model;
 
+import java.io.Serial;
 import java.io.Serializable;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "marca_produto")
 @SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
 public class MarcaProduto implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+	@Serial
+    private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
 	private Long id;
 
+	@Column(nullable = false)
 	private String nomeDesc;
 
 	public Long getId() {
